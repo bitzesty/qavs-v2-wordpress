bf498fe4e7b8241ee796a1ae3c6cdf9775bbec67
Mauricio Andre Cinelli <mauricio.andre.cinelli@gmail.com>
2021-05-18 11:18:37 -0300

    Update

--- wp-content/themes/qavs/template-parts/content.php
+++ wp-content/themes/qavs/template-parts/content.php
@@ -10,11 +10,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" class='news-article' >
-  <?php qavs_post_thumbnail(); ?>
+  <?php echo qavs_post_thumbnail(); ?>
   <div class="news-article__details">
     <?php the_title( '<h2 class="news-article__title" id="title-' . get_the_ID() . '">', '</a></h2>' ); ?>
     <div class="news-article__meta">
-      <?php qavs_posted_on(); ?>
+      <?php echo qavs_posted_on(); ?>
     </div>
     <a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark" aria-labelledby="title-<?php the_ID(); ?>" title="Click to read article" class="news-article__cta arrow-link">
       Read article
