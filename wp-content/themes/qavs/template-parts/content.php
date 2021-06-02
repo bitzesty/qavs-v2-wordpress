f09a697c6941b0d04a46835f6d6e5595e03c44c2
Mauricio Andre Cinelli <mauricio.cinelli@bitzesty.com>
2021-06-02 09:12:45 -0300

    Fix link stray tags

--- wp-content/themes/qavs/template-parts/content.php
+++ wp-content/themes/qavs/template-parts/content.php
@@ -12,7 +12,7 @@
 <article id="post-<?php the_ID(); ?>" class='news-article' >
   <?php echo qavs_post_thumbnail(get_the_ID()); ?>
   <div class="news-article__details">
-    <?php the_title( '<h2 class="news-article__title" id="title-' . get_the_ID() . '">', '</a></h2>' ); ?>
+    <?php the_title( '<h2 class="news-article__title" id="title-' . get_the_ID() . '">', '</h2>' ); ?>
     <div class="news-article__meta">
       <?php echo qavs_posted_on(get_the_ID()); ?>
     </div>
