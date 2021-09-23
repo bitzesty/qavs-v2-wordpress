60e696d79f6b2365fff0cc718c828ed5f131be92
Mauricio Andre Cinelli <mauricio.cinelli@bitzesty.com>
2021-09-23 05:03:19 -0300

    Auto did not work, setting root level

--- wp-content/themes/qavs/header.php
+++ wp-content/themes/qavs/header.php
@@ -22,7 +22,7 @@ $pageLanguage = carbon_get_post_meta(get_the_ID(), "page_language");
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
 
-      gtag('config', 'G-M130LYYCBV', { 'anonymize_ip': true, cookieDomain: 'auto' });
+      gtag('config', 'G-M130LYYCBV', { 'anonymize_ip': true, cookieDomain: '.dcms.gov.uk' });
     </script>
   <?php endif; ?>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
