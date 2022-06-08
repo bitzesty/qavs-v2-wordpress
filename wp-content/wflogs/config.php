8a204479b5f7f37ca7d938ec05a69e7ab4c911ca
DaniBitZesty <daniela.sinclair@bitzesty.com>
2022-06-08 14:08:56 +0100

    Adds wordpress sentry, wordfence security and wpforms lite plugins

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1655298461;s:7:"authKey";s:64:",Qg0z:hpfk8L+8B&#{YC`Dc|q5L_8pvR$}#cGS(MXwwX]*0~uZjX0?;%Uyj*iCr-";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;}
