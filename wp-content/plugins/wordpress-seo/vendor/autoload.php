a3164385f5edf7390b8a01a75cb8601df545b4d2
DaniBitZesty <daniela.sinclair@bitzesty.com>
2022-06-08 12:27:12 +0100

    Updating with plugins from prod

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit5872f17d2dfe666d2162bbc5c676a4dc::getLoader();
+return ComposerAutoloaderInit654002643e467df00eb8708ac77efa91::getLoader();
